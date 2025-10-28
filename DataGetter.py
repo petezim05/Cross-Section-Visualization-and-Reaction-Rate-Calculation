ad2fa6ff2aa2d077262351a1cb3d50002545c66e
petezim05 <petezim05@gmail.com>
2025-10-27 21:43:14 -0500

    Add files via upload

---
+++ DataGetter.py
@@ -0,0 +1,8 @@
+import numpy as np
+import endf
+import requests
+import os
+
+mat = endf.Material('n-092_U_235.endf')
+
+
---
+++ plotter.py
@@ -0,0 +1,4 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from scipy.interpolate import interp1d
